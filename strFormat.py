7d81848675ae5fc27a97691fb6537ce84202ee8c
vitalaefimov <vitok153@gmail.com>
2026-03-16 21:00:56 +0700

    17 string format

---
+++ strFormat.py
@@ -0,0 +1,6 @@
+print('My name is {}, I was born in {}, I work as a {}.'.format('Timur', 1992, 'programmer')) # My name is Timur, I was born in 1992, I work as a programmer.
+print('My name is {0}, I was born in {1}, I work as a {2}.'.format('Timur', 1992, 'programmer')) # My name is Timur, I was born in 1992, I work as a programmer.
+print('My name is {0}-{0}-{0}, I was born in {1}, I work as a {2}.'.format('Timur', 1992, 'programmer')) # My name is Timur-Timur-Timur, I was born in 1992, I work as a programmer.
+n = 5
+print(f'{n} + 2 = {n + 2}') # 5 + 2 = 7
+print(f'{'London'} is the capital of {'England'} and the {'United Kingdom'}') # Ограничений на разные кавычки нет!
