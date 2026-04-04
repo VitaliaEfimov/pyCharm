4df095a1dfc13e18d4922fc4700803ee821e349e
vitalaefimov <vitok153@gmail.com>
2026-04-04 21:27:18 +0700

    38 cezar code 1

---
+++ str/CezarCode1.py
@@ -0,0 +1,24 @@
+def get_chr(ind, islower, a, m):
+    if ind > m:
+        if islower:
+            return a[ind - m]
+        else:
+            return a[ind - m].upper()
+    else:
+        if islower:
+            return a[ind]
+        else:
+            return a[ind].upper()
+# a = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+a = 'abcdefghijklmnopqrstuvwxyz'
+s = input()
+r = ''
+rot = int(input())
+for i in s:
+    if i.isalpha():
+        islower = i == i.lower()
+        ind = a.find(i.lower()) + rot
+        r += get_chr(ind, islower, a, len(a))
+    else:
+        r += i
+print(r)
