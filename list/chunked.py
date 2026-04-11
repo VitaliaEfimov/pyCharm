770eeb39e37013af87d185b3f029e6c394b35834
vitalaefimov <vitok153@gmail.com>
2026-04-11 20:59:01 +0700

    50 chunked

---
+++ list/chunked.py
@@ -0,0 +1,10 @@
+def chunked(symbols, n):
+    result = []
+    for i in range(0, len(symbols), n):
+        result.append(symbols[i:i + n])
+    return result
+
+symbols = input().split()
+n = int(input())
+
+print(chunked(symbols, n))
